c37b0de7c51e93d8841efa5d2fc638e5c12dee25
sahanruwantha <sahanr.silva@hotmail.com>
2024-02-10 22:10:48 +0530

    bug fixes

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from util_array import pillam, charactors
 
 def to_sinhala_unicode(input_text):
     output_array = []
-    input_array = [char for char in input_text]
 
-    for i, char in enumerate(input_array):
+    for i, char in enumerate(input_text[:-1]):
         e_char = charactors.get(char)
         p_char = pillam.get(char)
 
@@ -13,19 +12,21 @@ def to_sinhala_unicode(input_text):
             output_array.append(char if char == ' ' else e_char)
 
             # Check for pillam and append 'a'
-            if i + 1 < len(input_text):
-                next_char = input_array[i + 1]
-                output_array.append(pillam.get(next_char) or 'a' if e_char and char != ' ' else '')
-
+            next_char = input_text[i + 1]
+            output_array.append(pillam.get(next_char) or 'a' if e_char and char != ' ' else '')
+        
         elif not e_char and not p_char:
             # Character not found in mapping, keep the original character
             output_array.append(char)
+            
+        if char == '්':
+            output_array.pop()
+
+    # Add the last character to the output
+    output_array.append(charactors.get(input_text[-1], input_text[-1]))
 
     return "".join(output_array)
 
-
-input_text = "පහත සඳහන් දෑ සතුන් ශාක සහ ඛනිජ ලෙස වර්ග කරන්න "
-
-output_text =  to_sinhala_unicode(input_text)
-
+input_text = "පහත සඳහන් දෑ සතුන්, ශාක සහ ඛනිජ ලෙස වර්ග කරන්න "
+output_text = to_sinhala_unicode(input_text)
 print(output_text)
