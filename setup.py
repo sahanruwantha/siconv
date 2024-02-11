0bd76329bdf7cd2107b3e70413a1f9d39aec88a0
sahanruwantha <sahanr.silva@hotmail.com>
2024-02-11 22:05:36 +0530

    bug fixes

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='siconv',
+version='0.1.0',
+author='sahan ruwantha',
+author_email='sahanr.silva@proton.me',
+description='',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
