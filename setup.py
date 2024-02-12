a316c85557049861c667cf0b90fceac920a9bd84
sahanruwantha <sahanr.silva@hotmail.com>
2024-02-12 08:14:35 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
+
 setup(
-name='siconv',
-version='0.1.0',
-author='sahan ruwantha',
-author_email='sahanr.silva@proton.me',
-description='''The siconv Python package simplifies the conversion between Sinhala and Singlish languages. Whether you're dealing with Sinhala text that needs transformation into Singlish or vice versa, siconv offers a user-friendly solution.Resources''',
-packages=find_packages(),
-classifiers=[
-'Programming Language :: Python :: 3',
-'License :: GNU GENERAL PUBLIC LICENSE',
-'Operating System :: OS Independent',
-],
-python_requires='>=3.6',
+    name='siconv',
+    version='0.1.0',
+    author='sahan ruwantha',
+    author_email='sahanr.silva@proton.me',
+    description='''The siconv Python package simplifies the conversion between Sinhala and Singlish languages. Whether you're dealing with Sinhala text that needs transformation into Singlish or vice versa, siconv offers a user-friendly solution.Resources''',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
